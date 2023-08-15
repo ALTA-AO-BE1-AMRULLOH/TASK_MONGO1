8f86a5630fc1e7bbabba7b8a9bec18b80fc1018f
amrulloh <amrullmisri@gmail.com>
2023-08-15 11:12:47 +0700

    NoSQL film

---
+++ film.sql
@@ -0,0 +1,537 @@
+{
+  "_id": {
+    "$oid": "64d9cb9fbfae7833cafd90ef"
+  },
+  "nama film": "jurassic",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "leonardo dicaprio"
+    },
+    {
+      " artist": "chakrakhan"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9a0f5bfae7833cafd90ee"
+  },
+  "nama film": "avatar",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": " jo william"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cc38bfae7833cafd90f0"
+  },
+  "nama film": "frozen",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "elizabhet"
+    },
+    {
+      " artist": "nurilahi"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cc65bfae7833cafd90f1"
+  },
+  "nama film": "dragon ball",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "goku"
+    },
+    {
+      " artist": "begita"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cca3bfae7833cafd90f2"
+  },
+  "nama film": "inception",
+  "category_film": [
+    {
+      "category": "action"
+    },
+    {
+      "category": "romance"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "leonardo dicaprio"
+    },
+    {
+      " artist": "chakrakhan"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9ccf5bfae7833cafd90f3"
+  },
+  "nama film": "gladiator",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "hanks"
+    },
+    {
+      " artist": "thomas"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cd36bfae7833cafd90f4"
+  },
+  "nama film": "gravity",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "neilson"
+    },
+    {
+      " artist": "rubidy"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cdb2bfae7833cafd90f5"
+  },
+  "nama film": "jaws",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "neilson"
+    },
+    {
+      " artist": "rubidy"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cdd5bfae7833cafd90f6"
+  },
+  "nama film": "grease",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "brandon"
+    },
+    {
+      " artist": "neilson"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cdf5bfae7833cafd90f7"
+  },
+  "nama film": "psycho",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "nabila"
+    },
+    {
+      " artist": "ria"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9ce21bfae7833cafd90f8"
+  },
+  "nama film": "armageddon",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "aldous"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9ce46bfae7833cafd90f9"
+  },
+  "nama film": "matrix",
+  "category_film": [
+    {
+      "category": "adventure"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "miya"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9ce6ebfae7833cafd90fa"
+  },
+  "nama film": "shrek",
+  "category_film": [
+    {
+      "category": "komedi"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "anastasya"
+    },
+    {
+      " artist": "jamil"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9ce94bfae7833cafd90fb"
+  },
+  "nama film": "memento",
+  "category_film": [
+    {
+      "category": "romance"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "jim ho"
+    },
+    {
+      " artist": "karina"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cecebfae7833cafd90fd"
+  },
+  "nama film": "elysium",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "jim ho"
+    },
+    {
+      " artist": "karina"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cf0cbfae7833cafd90fe"
+  },
+  "nama film": "prometheus",
+  "category_film": [
+    {
+      "category": "religi"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "huwang"
+    },
+    {
+      " artist": "kona"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cf27bfae7833cafd90ff"
+  },
+  "nama film": "whiplash",
+  "category_film": [
+    {
+      "category": "adventure"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "huwang"
+    },
+    {
+      " artist": "kona"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cf44bfae7833cafd9100"
+  },
+  "nama film": "pulpfiction",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "huwang"
+    },
+    {
+      " artist": "kona"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cf72bfae7833cafd9101"
+  },
+  "nama film": "incredibles",
+  "category_film": [
+    {
+      "category": "hiro"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "nurman"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cf91bfae7833cafd9102"
+  },
+  "nama film": "twilight",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "nurman"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9cfcabfae7833cafd9103"
+  },
+  "nama film": "minions",
+  "category_film": [
+    {
+      "category": "action"
+    },
+    {
+      "category": "komedi"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "gruth"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9d02abfae7833cafd9104"
+  },
+  "nama film": "madmax",
+  "category_film": [
+    {
+      "category": "automotif"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "jorgh"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9d230782bdcd7f8e17160"
+  },
+  "nama film": "alien",
+  "category_film": [
+    {
+      "category": "adventure"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "jorgh"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9d2a6782bdcd7f8e17161"
+  },
+  "nama film": "casino",
+  "category_film": [
+    {
+      "category": "komedi"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "jonathan"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9d2c1782bdcd7f8e17162"
+  },
+  "nama film": "shining",
+  "category_film": [
+    {
+      "category": "komedi"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "jonathan"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9d2fc782bdcd7f8e17163"
+  },
+  "nama film": "monster",
+  "category_film": [
+    {
+      "category": "adventure"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "jorgh"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9d312782bdcd7f8e17164"
+  },
+  "nama film": "love",
+  "category_film": [
+    {
+      "category": "romance"
+    }
+  ],
+  "cast": [
+    {
+      "artist": " jo william"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9d327782bdcd7f8e17165"
+  },
+  "nama film": "boyka",
+  "category_film": [
+    {
+      "category": "action"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "gruth"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9d344782bdcd7f8e17166"
+  },
+  "nama film": "sonic",
+  "category_film": [
+    {
+      "category": "action"
+    },
+    {
+      "category": "komedi"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "gruth"
+    }
+  ]
+}
+{
+  "_id": {
+    "$oid": "64d9d355782bdcd7f8e17167"
+  },
+  "nama film": "pokemon",
+  "category_film": [
+    {
+      "category": "action"
+    },
+    {
+      "category": "komedi"
+    }
+  ],
+  "cast": [
+    {
+      "artist": "gruth"
+    }
+  ]
+}
