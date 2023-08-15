fe2cc4c728d3ed71f12a03e2eeebb7088f8192d0
amrulloh <amrullmisri@gmail.com>
2023-08-15 11:23:47 +0700

    NoSQL

---
+++ category.sql
@@ -0,0 +1,180 @@
+{
+  "_id": {
+    "$oid": "64dae55b782bdcd7f8e1716f"
+  },
+  "category": "action"
+}
+{
+  "_id": {
+    "$oid": "64dae5af782bdcd7f8e17170"
+  },
+  "category": "komedi"
+}
+{
+  "_id": {
+    "$oid": "64dae5bd782bdcd7f8e17171"
+  },
+  "category": "drama"
+}
+{
+  "_id": {
+    "$oid": "64dae5da782bdcd7f8e17172"
+  },
+  "category": "science fiction"
+}
+{
+  "_id": {
+    "$oid": "64dae5ea782bdcd7f8e17173"
+  },
+  "category": "horror"
+}
+{
+  "_id": {
+    "$oid": "64dae5f5782bdcd7f8e17174"
+  },
+  "category": "romance"
+}
+{
+  "_id": {
+    "$oid": "64dae609782bdcd7f8e17175"
+  },
+  "category": "thriller"
+}
+{
+  "_id": {
+    "$oid": "64dae617782bdcd7f8e17176"
+  },
+  "category": "fantasy"
+}
+{
+  "_id": {
+    "$oid": "64dae620782bdcd7f8e17177"
+  },
+  "category": "adventure"
+}
+{
+  "_id": {
+    "$oid": "64dae629782bdcd7f8e17178"
+  },
+  "category": "animation"
+}
+{
+  "_id": {
+    "$oid": "64dae635782bdcd7f8e17179"
+  },
+  "category": "crime"
+}
+{
+  "_id": {
+    "$oid": "64daeffe782bdcd7f8e1717a"
+  },
+  "category": "mystery"
+}
+{
+  "_id": {
+    "$oid": "64daf012782bdcd7f8e1717b"
+  },
+  "category": "historical"
+}
+{
+  "_id": {
+    "$oid": "64daf01a782bdcd7f8e1717c"
+  },
+  "category": "war"
+}
+{
+  "_id": {
+    "$oid": "64daf025782bdcd7f8e1717d"
+  },
+  "category": "musical"
+}
+{
+  "_id": {
+    "$oid": "64daf034782bdcd7f8e1717e"
+  },
+  "category": "documentary"
+}
+{
+  "_id": {
+    "$oid": "64daf048782bdcd7f8e1717f"
+  },
+  "category": "biogrphy"
+}
+{
+  "_id": {
+    "$oid": "64daf051782bdcd7f8e17180"
+  },
+  "category": "family"
+}
+{
+  "_id": {
+    "$oid": "64daf066782bdcd7f8e17181"
+  },
+  "category": "western"
+}
+{
+  "_id": {
+    "$oid": "64daf073782bdcd7f8e17182"
+  },
+  "category": "superhero"
+}
+{
+  "_id": {
+    "$oid": "64daf07b782bdcd7f8e17183"
+  },
+  "category": "sport"
+}
+{
+  "_id": {
+    "$oid": "64daf083782bdcd7f8e17184"
+  },
+  "category": "spy"
+}
+{
+  "_id": {
+    "$oid": "64daf098782bdcd7f8e17185"
+  },
+  "category": "supernatural"
+}
+{
+  "_id": {
+    "$oid": "64daf0a8782bdcd7f8e17186"
+  },
+  "category": "disaster"
+}
+{
+  "_id": {
+    "$oid": "64daf0b1782bdcd7f8e17187"
+  },
+  "category": "political"
+}
+{
+  "_id": {
+    "$oid": "64daf0cd782bdcd7f8e17188"
+  },
+  "category": "coming_of_age"
+}
+{
+  "_id": {
+    "$oid": "64daf0d7782bdcd7f8e17189"
+  },
+  "category": "musical_comedy"
+}
+{
+  "_id": {
+    "$oid": "64daf0e9782bdcd7f8e1718a"
+  },
+  "category": "cyberpunk"
+}
+{
+  "_id": {
+    "$oid": "64daf0f0782bdcd7f8e1718b"
+  },
+  "category": "noir"
+}
+{
+  "_id": {
+    "$oid": "64daf103782bdcd7f8e1718c"
+  },
+  "category": "fantasy comedy"
+}
